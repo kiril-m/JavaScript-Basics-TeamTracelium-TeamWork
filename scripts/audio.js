b7cfcf19bba7190a75b989d6382844bcc8bb6285
ValsPenev <vruzkata@gmail.com>
2014-11-20 05:58:40 +0200

    малко поправки преди лягане :D

--- scripts/audio.js
+++ scripts/audio.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function () {
     var audio = document.getElementById('music');
-    $('body').keyup(function(e) {
+    $('body').keydown(function(e) {
         if (e.keyCode == 77) {
             console.log(audio.volume);
             if (audio.volume == 0.0) { //mute button
@@ -16,24 +16,13 @@ $(document).ready(function () {
         else if (e.keyCode == 73) { //volume up
             console.log(audio.volume);
             if (audio.volume < 1.0) {
-                if (audio.volume == 0.0){
-                    audio.volume = 0.5;
-                }
-                else {
                     audio.volume += 0.05;
-                }
-            }
-            else  if (audio.volume == 1.0){
-                audio.volume = 1.0;
             }
         }
         else if (e.keyCode == 75) { //volume down
             console.log(audio.volume);
             if (audio.volume > 0.0) {
                 audio.volume -= 0.05;
-            }
-            else  if (audio.volume == 0.0){
-                audio.volume = 0.0;
             }
         }
     })
